d8c4addc01263c3010b345f1ebfa77d6c8ae43cc
Zelbot <Zelbot@users.noreply.github.com>
2024-01-18 15:37:16 +0100

    Add mod utilities / dependencies

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 # BUILTIN
 import os
 
-
 lethal_company_install_directory = 'S:\Games\Steam\steamapps\common\Lethal Company'
 
 mods_directory = os.path.join(os.getcwd(), 'mods')
@@ -22,4 +21,8 @@ thunderstore_mod_urls = [
     # Quality of life
     'https://thunderstore.io/c/lethal-company/p/Renegades/FlashlightToggle/',
     'https://thunderstore.io/c/lethal-company/p/tinyhoot/ShipLoot/',
+    # Mod utilities / dependencies
+    'https://thunderstore.io/c/lethal-company/p/Rune580/LethalCompany_InputUtils/',
+    'https://thunderstore.io/c/lethal-company/p/Ozone/Runtime_Netcode_Patcher/',
+    'https://thunderstore.io/c/lethal-company/p/AlexCodesGames/AdditionalContentFramework/',
 ]
